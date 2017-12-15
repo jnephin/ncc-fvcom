759749f03b711ba7050f500ca7b670c8be27fe2a
jnephin <jessica.nephin@dfo-mpo.gc.ca>
2017-12-15 11:03:41 -0800

    udpated spline script

--- SplineBarriers.py
+++ SplineBarriers.py
@@ -5,6 +5,8 @@
 # Requirements: Spatial Analyst Extension and Java Runtime
 # Author: Jessica Nephin
 
+# Run from parent directory
+
 # Import system modules
 import os
 import arcpy
@@ -15,7 +17,6 @@ from arcpy.sa import *
 arcpy.CheckOutExtension("Spatial")
 
 # set workspace
-os.chdir("..")
 #os.chdir("E:/AbioticData/Ocean_Models/Pramod_NSB_FVCOM_2017")
 
 # Set local variables
@@ -29,15 +30,17 @@ for f in fields:
 
     # inputs
     zField = f.name
-    inBarrierFeature =  os.getcwd()+"/Boundary/NCC_Area_20mbuffer_15ksponge.shp"
-    cellSize =  20.0
+    inBarrierFeature =  os.getcwd()+"/Boundary/NCC_Coastline_Simplified_15kmsponge_50mbuffer.shp"
+    inMaskData = os.getcwd()+"/Boundary/NCC_Area_15ksponge.shp"
+    outfilename = os.getcwd()+"/Data/Interp/Rasters/FVCOM_"+zField+"_20m.tif"
+    cellSize =  100.0
     smoothing = 1
 
     # Execute Spline with Barriers
     outSB = SplineWithBarriers(infeature, zField, inBarrierFeature, cellSize, smoothing)
 
-    # Use splitext to set the output name
-    outfilename = os.getcwd()+"/Data/Interp/Rasters/FVCOM_"+zField+"_20m.tif"
+    # Execute ExtractByMask
+    outExtractByMask = ExtractByMask(outSB, inMaskData)
 
     # Save the output
-    outSB.save(outfilename)
+    outExtractByMask.save(outfilename)
